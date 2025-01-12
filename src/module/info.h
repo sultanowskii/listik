7f397e20e095f2da5c1d1204dbc2bb58f67870b5
sultanowskii <dsr431@yandex.ru>
2025-01-12 18:39:12 +0300

    fix indentation

--- src/module/info.h
+++ src/module/info.h
@@ -1,11 +1,11 @@
 #pragma once
 
 #if defined(__x86_64__) || defined(__aarch64__)
-    #define CPU_MODES "32-bit, 64-bit"
+#define CPU_MODES "32-bit, 64-bit"
 #elif defined(__i386__) || defined(__arm__)
-    #define CPU_MODES "32-bit"
+#define CPU_MODES "32-bit"
 #else
-    #define CPU_MODES "64-bit"
+#define CPU_MODES "64-bit"
 #endif
 
 const char *get_endianness(void);
